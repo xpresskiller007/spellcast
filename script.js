ff4ecdb16b31190fb155e38cc2af03ecd282b6f0
xpresskiller007 <xpresskiller007@yandex.ru>
2024-03-15 20:49:03 +0300

    Заготовка под каст спелла

--- script.js
+++ script.js
@@ -24,6 +24,43 @@ var drgY;
 var map;
 
 var scene_main;
+var scene_CastFrame;
+
+
+class CastFrame extends Phaser.Scene {
+    
+    create(){
+        scene_CastFrame = this;
+
+        this.castopen = false;
+        this.graphics = this.add.graphics();
+        this.ClsdBtn = this.add.sprite(windowInnerWidth - 50, windowInnerHeight - 200, 'ClsdBtn').setInteractive();
+        this.ClsdBtn.visible = false;
+        this.ClsdBtn.on('pointerdown', function (pointer, gameObject) {
+            scene_CastFrame.close()
+        });
+
+    }
+
+    open(){
+
+        let xsize = windowInnerWidth / 2 - 200;
+        let ysize = windowInnerHeight / 2 - 200;
+
+        this.graphics.fillStyle(0x0000ff, 0.5);
+        this.graphics.fillRect(xsize, ysize, 200, 200);
+        this.ClsdBtn.visible = true;
+        this.ClsdBtn.setPosition(xsize + 200 - 10, ysize - 10)
+        this.castopen = true;
+    }
+
+    close(){
+        this.ClsdBtn.visible = false;
+        this.graphics.clear();
+        this.castopen = false
+    }
+}
+
 class MainScene extends Phaser.Scene {
 
     preload() {
@@ -62,10 +99,20 @@ class MainScene extends Phaser.Scene {
         const tiles = map.addTilesetImage('SummerTiles', 'SummerTiles')
         const layer = map.createLayer('layer1', tiles, 0, 0);
 
-
         this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
 
-        cursors = this.input.keyboard.createCursorKeys();
+        this.castframe = this.add.sprite(windowInnerWidth-100, windowInnerHeight-100, 'bomb').setScale(3).setInteractive();
+
+        scene_main.scene.add('CastFrame', CastFrame, true, { x: 400, y: 300 });
+
+        this.castframe.on('pointerdown', function (pointer, gameObject) {
+
+            if (!scene_CastFrame.castopen)
+            scene_CastFrame.open()
+
+        });
+
+        cursors = this.input.keyboard.createCursorKeys(); 
 
         this.input.on('pointerdown', function (pointer, gameObject) {
 
@@ -73,7 +120,7 @@ class MainScene extends Phaser.Scene {
 
 
             }
-            
+
         });
 
 
